a74bac2aa6cc614ee361ded03dd3a4dcb6c0d405
Fernando Cezar Vicari <fcvicari@gmail.com>
2021-03-04 17:15:28 -0300

    Finalizada a função do countdown

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,9 +1,14 @@
-import { Button, Container, Grid, Text } from '@chakra-ui/react';
+import { Button, Center, Container, Grid, Icon, Text } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 
+import { FaCheckCircle } from 'react-icons/fa';
+
 export function Countdown() {
-  const [time, setTime] = useState(25 * 60);
-  const [active, setActive] = useState(false);
+  let countdownTimeout: NodeJS.Timeout;
+
+  const [time, setTime] = useState(0.05 * 60);
+  const [isActive, setIsActive] = useState(false);
+  const [hasFinish, setHasFinish] = useState(false);
 
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
@@ -11,30 +16,33 @@ export function Countdown() {
   const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
 
   function startCountdown() {
-    setActive(!active);
-    if (active) {
-      setTime(25 * 60);
+    setIsActive(!isActive);
+    if (isActive) {
+      clearTimeout(countdownTimeout);
+      setTime(0.05 * 60);
     }
   }
 
   useEffect(() => {
-    if (active && time > 0) {
-      setTimeout(() => {
+    if (hasFinish) {
+      console.log('finalizou');
+    }
+  }, [hasFinish]);
+
+  useEffect(() => {
+    if (isActive && time > 0) {
+      countdownTimeout = setTimeout(() => {
         setTime(time - 1);
       }, 1000);
+    } else if (isActive && time === 0) {
+      setIsActive(false);
+      setHasFinish(true);
     }
-  }, [active, time]);
+  }, [isActive, time]);
 
   return (
     <Grid>
-      <Container
-        variant="countdownContainer"
-        margin="0"
-        padding="0"
-        flex="1"
-        justifyContent="center"
-        alignItems="center"
-      >
+      <Container variant="countdownContainer" margin="0" padding="0" flex="1">
         <Container variant="countdownNumber">
           <Text as="span" variant="countdownRight">
             {minuteLeft}
@@ -55,15 +63,32 @@ export function Countdown() {
           </Text>
         </Container>
       </Container>
-      <Button
-        onClick={startCountdown}
-        marginTop="2rem"
-        w="100%"
-        h="5rem"
-        variant={!active ? 'start' : 'stop'}
-      >
-        {!active ? 'Iniciar ciclo' : 'Abandonar ciclo'}
-      </Button>
+      {hasFinish ? (
+        <Container marginTop="2rem" w="100%" h="5rem" variant="countdownFinish">
+          <Center>
+            Ciclo encerrado
+            <Icon
+              name="Finish"
+              as={FaCheckCircle}
+              w={6}
+              h={6}
+              color="green.500"
+              marginLeft="1rem"
+            />
+          </Center>
+        </Container>
+      ) : (
+        <Container marginTop="2rem" w="100%" h="5rem" variant="countdownButton">
+          <Button
+            onClick={startCountdown}
+            w="100%"
+            h="5rem"
+            variant={!isActive ? 'start' : 'stop'}
+          >
+            {!isActive ? 'Iniciar ciclo' : 'Abandonar ciclo'}
+          </Button>
+        </Container>
+      )}
     </Grid>
   );
 }
